5d7bca91c0df2e107cd9035534124386c96ee71c
Percival Gebashe <percivalgebashe@gmail.com>
2024-12-03 08:41:00 +0200

    Exercise complete

--- src/main/java/com/linkedin/collections/Application.java
+++ src/main/java/com/linkedin/collections/Application.java
@@ -4,11 +4,15 @@ public class Application {
 
 	public static void main(String[] args) {
 
-		
+		Contract contract = new Implementation();
+
+		printTerms(contract);
 	}
 
 	private static void printTerms(Contract contract) {
-		
+		contract.term1();
+		contract.term2();
+		contract.extendedTerm();
 	}
 
 }
--- src/main/java/com/linkedin/collections/Implementation.java
+++ src/main/java/com/linkedin/collections/Implementation.java
@@ -3,4 +3,18 @@ package com.linkedin.collections;
 public class Implementation implements Contract  {
 
 
+    @Override
+    public void term1() {
+        System.out.println("Imp:Term1");
+    }
+
+    @Override
+    public void term2() {
+        System.out.println("Imp:Term2");
+    }
+
+    @Override
+    public void extendedTerm() {
+        System.out.println("Imp:ExtendedTerm");
+    }
 }
