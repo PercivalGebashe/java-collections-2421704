2c2f2336f2cbaa48faa8987a799f7d72d5f008fe
Percival Gebashe <percivalgebashe@gmail.com>
2024-12-03 08:15:34 +0200

    Exercise comple

--- src/main/java/com/linkedin/collections/Application.java
+++ src/main/java/com/linkedin/collections/Application.java
@@ -1,18 +1,27 @@
 package com.linkedin.collections;
 
+import java.util.Collection;
+import java.util.List;
+
 public class Application {
 
 	public static void main(String[] args) {
 	
 		Room cambridge = new Room("Cambridge", "Premiere Room", 4, 175.00);
 		Room manchester = new Room("Manchester", "Suite", 5, 250.0);
+		Room soweto = new Room("Soweto", "Premiere Room", 4, 450.00);
+		Room alex = new Room("Alex", "Suite", 5, 1000.00);
 
-		double total = getPotentialRevenue(cambridge, manchester);
+		Collection<Room> rooms = List.of(cambridge, manchester, soweto, alex);
+
+		double total = getPotentialRevenue(rooms);
 		System.out.println(total);
 	}
 	
-	private static double getPotentialRevenue(Room room1, Room room2) {
-		return room1.getRate() + room2.getRate();
+	private static double getPotentialRevenue(Collection<Room> rooms) {
+		return rooms.stream()
+				.mapToDouble(Room::getRate)
+				.sum();
 	}
 
 }
